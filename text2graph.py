3f70db28aa76e152e5d6b869e6966bd52bdd342f
Benedikt Franke <benefranke@t-online.de>
2020-12-13 15:02:56 +0100

    edge weight matrices

--- text2graph.py
+++ text2graph.py
@@ -2,7 +2,7 @@ from typing import Union, List
 import glob
 import os
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 import nltk
 import torch as th
 import joblib as jl
@@ -11,7 +11,7 @@ import joblib as jl
 class Text2Graph(BaseEstimator, TransformerMixin):
     valid_stopwords = ['sklearn', 'nltk']
 
-    def __init__(self, word_threshold: int = 5, save_path: str = None, n_jobs: int = 1):
+    def __init__(self, word_threshold: int = 5, window_size: int = 20, save_path: str = None, n_jobs: int = 1):
         self.n_jobs = n_jobs
         nltk.download('punkt')
         # assert isinstance(stopwords, list) or stopwords in self.valid_stopwords
@@ -20,10 +20,11 @@ class Text2Graph(BaseEstimator, TransformerMixin):
         self.save_path = save_path
         self.input = None
         self.cv = None
+        self.window_size = window_size
 
     def fit_transform(self, X, y=None, mask=None, **fit_params):
         # load the text
-        if isinstance(input, list):
+        if isinstance(X, list):
             self.input = X
         else:
             self.input = []
@@ -35,10 +36,13 @@ class Text2Graph(BaseEstimator, TransformerMixin):
         occurrence_mat = self.cv.fit_transform(self.input).toarray()
         del self.input
         # build the graph
-        # TODO: do NOT forget to call to_undirected at the end of this, else we have an directed graph
         # id-matrix of size n_vocab + n_docs
         n_docs, n_vocabs = occurrence_mat.shape
         node_feats = th.eye(n_docs + n_vocabs)
+        # memory-intensive solution: compute PMI and TFIDF matrices and store them
+        tfidf_mat = TfidfTransformer().fit_transform(occurance_mat)
+        pmi_mat = ...
+
         # build word-document edges. The first value is increased by n_vocab, as documents start at index n_vocab
         docu_coo = th.nonzero(occurrence_mat) + th.Tensor([n_vocabs, 0])
         # build word-word edges
@@ -46,5 +50,5 @@ class Text2Graph(BaseEstimator, TransformerMixin):
             jl.delayed(lambda i, x: th.hstack([th.nonzero(x), th.Tensor([[i + n_vocabs]] * len(th.nonzero(x)))]))
             for i, x in enumerate(occurrence_mat)
         ))
-        coo = th.vstack([docu_coo, word_coo])
+        coo = th.vstack([word_coo, docu_coo])
         g = ...         # set up tg.data object
