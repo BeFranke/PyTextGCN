852e2180c3ada2ab8d5c16299a3479662393c9c2
Fabian Wurzberger <fabi.wurz@gmail.com>
2021-01-01 12:20:40 +0100

    added validation idx to Text2GraphTransformer

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os
+import time
 from datetime import datetime
 
 import numpy as np
@@ -10,33 +11,45 @@ from sklearn.preprocessing import LabelEncoder
 
 from textgcn import GCN, Text2GraphTransformer
 
-CPU_ONLY = True
+CPU_ONLY = False
 EARLY_STOPPING = False
-epochs = 200
+epochs = 500
+train_val_split = 0.1
 
-train = pd.read_csv("data/amazon/train_40k.csv")
-# save_path="textgcn/graphs/"
-save_path = None
+train = pd.read_csv("data/amazon/train.csv")
+test = pd.read_csv("data/amazon/test.csv")
 
+save_path = "textgcn/graphs/"
+# save_path = None
 x = train['Text'].tolist()
 y = train['Cat1'].tolist()
 
-y = LabelEncoder().fit_transform(y)
+# Train/val split
+val_idx = np.random.choice(len(x), int(train_val_split * len(x)), replace=False)
+train_idx = np.array([x for x in range(len(x)) if x not in val_idx])
 
-test_idx = np.random.choice(len(x), int(0.1 * len(x)), replace=False)
-train_idx = np.array([x for x in range(len(x)) if x not in test_idx])
+x_test = test['Text'].tolist()
+y_test = test['Cat1'].tolist()
+
+test_idx = np.arange(len(x)+1, len(x)+ len(x_test))
+
+# Combine training & test data set
+x = x + x_test
+y = y + y_test
+
+y = LabelEncoder().fit_transform(y)
 print("Data loaded!")
 
 t2g = Text2GraphTransformer(n_jobs=8, min_df=5, save_path=save_path, verbose=1, max_df=0.9)
 ls = os.listdir("textgcn/graphs")
 if not ls:
-    g = t2g.fit_transform(x, y, test_idx=test_idx)
+    g = t2g.fit_transform(x, y, test_idx=test_idx, val_idx=val_idx)
 else:
     g = t2g.load_graph(os.path.join("textgcn/graphs", ls[0]))
 
 print("Graph built")
 
-gcn = GCN(g.x.shape[1], len(np.unique(y)), n_hidden_gcn=200)
+gcn = GCN(g.x.shape[1], len(np.unique(y)), n_hidden_gcn=50)
 
 criterion = th.nn.CrossEntropyLoss(reduction='mean')
 
@@ -60,27 +73,27 @@ for epoch in range(epochs):
     optimizer.step()
     gcn.eval()
     with th.no_grad():
-        predictions = np.argmax(gcn(g)[g.test_mask].cpu().numpy(), axis=1)
+        pred_val = np.argmax(gcn(g)[g.val_mask].cpu().numpy(), axis=1)
         pred_train = np.argmax(gcn(g)[g.train_mask].cpu().numpy(), axis=1)
-        acc = accuracy_score(g.y.cpu()[g.test_mask].detach(), predictions)
+        acc_val = accuracy_score(g.y.cpu()[g.val_mask].detach(), pred_val)
         acc_train = accuracy_score(g.y.cpu()[g.train_mask].detach(), pred_train)
         print(f"[{epoch + 1:{length}}] loss: {loss.item(): .3f}, "
-              f"training accuracy: {acc_train: .3f}, val_accuracy: {acc: .3f}")
-    history.append((loss.item(), acc))
+              f"training accuracy: {acc_train: .3f}, val_accuracy: {acc_val: .3f}")
+    history.append((loss.item(), acc_val))
     if epoch > 5 and history[-5][0] < history[-1][0] and EARLY_STOPPING:
         print("early stopping activated!")
         break
 
 print("Optimization finished!")
 print("Saving model...")
-th.save(gcn, f"models/gcn_{datetime.now()}.nn")
+th.save(gcn, f"models/gcn_{int(time.time())}.nn")
 with th.no_grad():
-    predictions = np.argmax(gcn(g)[g.test_mask].cpu().detach().numpy(), axis=1)
-    acc = accuracy_score(g.y.cpu()[g.test_mask].detach(), predictions)
-    f1 = f1_score(g.y.cpu()[g.test_mask].detach(), predictions, average='macro')
-    conf_mat = confusion_matrix(g.y.cpu()[g.test_mask].detach(), predictions)
+    pred_test = np.argmax(gcn(g)[g.test_mask].cpu().detach().numpy(), axis=1)
+    acc_test = accuracy_score(g.y.cpu()[g.test_mask].detach(), pred_test)
+    f1 = f1_score(g.y.cpu()[g.test_mask].detach(), pred_test, average='macro')
+    conf_mat = confusion_matrix(g.y.cpu()[g.test_mask].detach(), pred_test)
 
-print(f"Test Accuracy: {acc: .3f}")
+print(f"Test Accuracy: {acc_test: .3f}")
 print(f"F1-Macro: {f1: .3f}")
 print("Confusion matrix:")
 print(conf_mat)
