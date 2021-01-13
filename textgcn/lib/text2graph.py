587e613f6b636513be6c023b8301e9ec1d8a1ba4
Tobias <tobias-1.zielke@uni-ulm.de>
2021-01-13 11:11:14 +0100

    fastHypOpt

--- textgcn/lib/text2graph.py
+++ textgcn/lib/text2graph.py
@@ -11,7 +11,7 @@ import torch_geometric as tg
 import nltk
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from tqdm.asyncio import tqdm
+from tqdm import tqdm
 from scipy import sparse as sp
 
 from .clib.graphbuilder import compute_word_word_edges
