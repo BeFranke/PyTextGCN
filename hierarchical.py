1fb68d5074c5638d1082c9c5368bc952caaa3055
Benedikt Franke <benedikt.franke@uni-ulm.de>
2021-02-08 14:21:17 +0100

    evaluation script for per-level

--- hierarchical.py
+++ hierarchical.py
@@ -20,9 +20,20 @@ train_val_split = 0.1
 lr = 0.005
 save_model = False
 dropout = 0.6
+model = GCN
+max_df = 0.7
 
-# set to "false" to evaluate flat approach
-hierarchical_feats = True
+seed = 42
+result_file = "results.csv"
+
+np.random.seed(seed)
+th.random.manual_seed(seed)
+
+try:
+    df = pd.read_csv(result_file)
+except:
+    df = pd.DataFrame(columns=["seed", "model", "hierarchy", "f1-macro", "accuracy"])
+
 
 train = pd.read_csv("data/amazon/train.csv")
 test = pd.read_csv("data/amazon/test.csv")
@@ -51,44 +62,39 @@ y = LabelEncoder().fit_transform(y)
 y_top = LabelEncoder().fit_transform(y_top)
 print("Data loaded!")
 
-t2g = Text2GraphTransformer(n_jobs=8, min_df=5, save_path=save_path, verbose=1, max_df=0.6)
-hierarchy = OneHotEncoder(sparse=False).fit_transform(y_top.reshape(-1, 1))
+t2g = Text2GraphTransformer(n_jobs=8, min_df=5, save_path=save_path, verbose=1, max_df=max_df)
 
 
-g = t2g.fit_transform(x, y, test_idx=test_idx, val_idx=val_idx,
-                      hierarchy_feats=hierarchy if hierarchical_feats else None)
+g1 = t2g.fit_transform(x, y_top, test_idx=test_idx, val_idx=val_idx, hierarchy_feats=None)
+
 print("Graph built!")
 
-gcn = EGCN(g.x.shape[1], len(np.unique(y)), n_hidden_gcn=100, embedding_dim=2000, dropout=dropout)
-# gcn = GCN(g.x.shape[1], len(np.unique(y)), n_hidden_gcn=64, n_gcn=2)
-
-# gcn = JumpingKnowledgeNetwork(g.x.shape[1], len(np.unique(y)), n_gcn=4, n_hidden_gcn=64, dropout=0.5, activation=th.nn.ReLU)
+gcn1 = model(g1.x.shape[1], len(np.unique(y)), n_hidden_gcn=100, dropout=dropout)
 
 criterion = th.nn.CrossEntropyLoss(reduction='mean')
 
 device = th.device('cuda' if th.cuda.is_available() and not CPU_ONLY else 'cpu')
-gcn = gcn.to(device).float()
-g = g.to(device)
+gcn1 = gcn1.to(device).float()
+g1 = g1.to(device)
 
 # optimizer needs to be constructed AFTER the model was moved to GPU
-optimizer = th.optim.Adam(gcn.parameters(), lr=lr)
-scheduler = th.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=5, cooldown=10, verbose=True)
-history = []
+optimizer = th.optim.Adam(gcn1.parameters(), lr=lr)
+
 length = len(str(epochs))
 print(device)
 print("#### TRAINING START ####")
 time_start = datetime.now()
 for epoch in range(epochs):
-    gcn.train()
-    outputs = gcn(g)[g.train_mask]
-    loss = criterion(outputs, g.y[g.train_mask])
+    gcn1.train()
+    outputs = gcn1(g1)[g1.train_mask]
+    loss = criterion(outputs, g1.y[g1.train_mask])
     # performance tip: try set_to_none=True
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
-    gcn.eval()
+    gcn1.eval()
     with th.no_grad():
-        logits = gcn(g)
+        logits = gcn1(g1)
         val_loss = criterion(logits[g.val_mask], g.y[g.val_mask])
         pred_val = np.argmax(logits[g.val_mask].cpu().numpy(), axis=1)
         pred_train = np.argmax(logits[g.train_mask].cpu().numpy(), axis=1)
@@ -96,23 +102,47 @@ for epoch in range(epochs):
         acc_train = accuracy_score(g.y.cpu()[g.train_mask], pred_train)
         print(f"[{epoch + 1:{length}}] loss: {loss.item(): .3f}, "
               f"training accuracy: {acc_train: .3f}, val_accuracy: {acc_val: .3f}")
-    history.append((loss.item(), acc_val))
 
-    # scheduler.step(val_loss)
-
-    if epoch > 5 and history[-5][0] < history[-1][0] and EARLY_STOPPING:
-        print("early stopping activated!")
-        break
-
-print("Optimization finished!")
-if save_model:
-    print("Saving model...")
-    th.save(gcn, f"models/gcn_{int(time.time())}.nn")
 with th.no_grad():
-    pred_test = np.argmax(gcn(g)[g.test_mask].cpu().detach().numpy(), axis=1)
-    acc_test = accuracy_score(g.y.cpu()[g.test_mask].detach(), pred_test)
-    f1 = f1_score(g.y.cpu()[g.test_mask].detach(), pred_test, average='macro')
-    conf_mat = confusion_matrix(g.y.cpu()[g.test_mask].detach(), pred_test)
+    hierarchy = gcn1(g1).cpu().numpy()
+
+g2 = t2g.fit_transform(x, y, test_idx=test_idx, val_idx=val_idx, hierarchy_feats=hierarchy)
+gcn2 = model(g2.x.shape[1], len(np.unique(y)), n_hidden_gcn=100, dropout=dropout)
+
+gcn2 = gcn2.to(device).float()
+g2 = g2.to(device)
+
+# optimizer needs to be constructed AFTER the model was moved to GPU
+optimizer = th.optim.Adam(gcn2.parameters(), lr=lr)
+
+length = len(str(epochs))
+print(device)
+print("#### TRAINING START ####")
+time_start = datetime.now()
+for epoch in range(epochs):
+    gcn2.train()
+    outputs = gcn2(g2)[g2.train_mask]
+    loss = criterion(outputs, g2.y[g2.train_mask])
+    # performance tip: try set_to_none=True
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+    gcn2.eval()
+    with th.no_grad():
+        logits = gcn2(g2)
+        val_loss = criterion(logits[g2.val_mask], g2.y[g2.val_mask])
+        pred_val = np.argmax(logits[g2.val_mask].cpu().numpy(), axis=1)
+        pred_train = np.argmax(logits[g2.train_mask].cpu().numpy(), axis=1)
+        acc_val = accuracy_score(g2.y.cpu()[g2.val_mask], pred_val)
+        acc_train = accuracy_score(g2.y.cpu()[g2.train_mask], pred_train)
+        print(f"[{epoch + 1:{length}}] loss: {loss.item(): .3f}, "
+              f"training accuracy: {acc_train: .3f}, val_accuracy: {acc_val: .3f}")
+
+with th.no_grad():
+    pred_test = np.argmax(gcn2(g2)[g2.test_mask].cpu().detach().numpy(), axis=1)
+    acc_test = accuracy_score(g2.y.cpu()[g2.test_mask].detach(), pred_test)
+    f1 = f1_score(g2.y.cpu()[g2.test_mask].detach(), pred_test, average='macro')
+    conf_mat = confusion_matrix(g2.y.cpu()[g2.test_mask].detach(), pred_test)
 
 print(f"Test Accuracy: {acc_test: .3f}")
 print(f"F1-Macro: {f1: .3f}")
@@ -122,11 +152,6 @@ print(conf_mat)
 time_end = datetime.now()
 print(f"Training took {time_end - time_start} for {epoch + 1} epochs.")
 
-loss, acc = zip(*history)
-
-fig, axs = plt.subplots(2)
-axs[0].plot(loss, label="TrainLoss")
-axs[1].plot(acc, label="ValAcc")
-axs[0].legend()
-axs[1].legend()
-plt.show()
+i = df.index.max() + 1 if df.index.max() != np.nan else 0
+df.loc[i] = [seed, "GCN" if isinstance(gcn2, GCN) else "EGCN", "flat", f1, acc_test]
+df.to_csv(result_file)
