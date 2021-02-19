ab11d5624a77f784b51a0aab8973d299942cc5dd
Benedikt Franke <benedikt.franke@uni-ulm.de>
2021-02-19 12:49:35 +0100

    MLP baseline per-level

--- MLP_level_amazon.py
+++ MLP_level_amazon.py
@@ -3,9 +3,10 @@ from typing import List
 import numpy as np
 import torch as th
 import pandas as pd
+from scipy import sparse as sp
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import f1_score, accuracy_score
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 """
 TODO
@@ -16,6 +17,19 @@ def csr_to_torch(csr):
     return th.sparse_coo_tensor(th.LongTensor([acoo.row.tolist(), acoo.col.tolist()]),
                                 th.FloatTensor(acoo.data.astype(np.float32)),
                                 size=csr.shape)
+
+
+def append_feats(feats, top_labels):
+    feats = feats.cpu()
+    if isinstance(top_labels, np.ndarray):
+        top_labels = th.from_numpy(top_labels)
+    indices = th.nonzero(top_labels).t()
+    values = top_labels[indices[0], indices[1]]
+    if isinstance(top_labels, np.ndarray):
+        values = th.from_numpy(values)
+    mat = th.sparse.FloatTensor(indices, values, top_labels.shape)
+    mat = mat.cpu()
+    return th.cat([feats, mat], dim=1)
 
 
 class MLP(th.nn.Module):
@@ -42,7 +56,7 @@ class MLP(th.nn.Module):
 
 CPU_ONLY = False
 EARLY_STOPPING = False
-epochs = 500
+epochs = 50
 train_val_split = 0.1
 lr = 0.05
 dropout = 0.7
@@ -120,30 +134,75 @@ model1 = MLP(x_train.shape[1], len(np.unique(y_top_train)), [256, 128], dropout=
 criterion = th.nn.CrossEntropyLoss(reduction='mean')
 
 device = th.device('cuda' if th.cuda.is_available() and not CPU_ONLY else 'cpu')
-model = model.to(device).float()
+model1 = model1.to(device).float()
 x_train = x_train.to(device)
-y_train = y_train.to(device)
+y_top_train = y_top_train.to(device)
 x_val = x_val.to(device)
 
 print(f"x_val shape: {x_val.shape}")
 print(f"x_train shape: {x_train.shape}")
 
 # optimizer needs to be constructed AFTER the model was moved to GPU
-optimizer = th.optim.Adam(model.parameters(), lr=lr)
+optimizer = th.optim.Adam(model1.parameters(), lr=lr)
 length = len(str(epochs))
 print("### Training start (Top-Level)! ###")
 for epoch in range(epochs):
-    model.train()
-    outputs = model(x_train)
+    model1.train()
+    outputs = model1(x_train)
+    loss = criterion(outputs, y_top_train)
+    # performance tip: try set_to_none=True
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+    model1.eval()
+    with th.no_grad():
+        logits = model1(x_train)
+        logits_val = model1(x_val)
+        pred_val = np.argmax(logits_val.cpu().numpy(), axis=1)
+        pred_train = np.argmax(logits.cpu().numpy(), axis=1)
+        f1_val = f1_score(y_top_val.cpu(), pred_val, average='macro')
+        acc_train = accuracy_score(y_top_train.cpu(), pred_train)
+        print(f"[{epoch + 1:{length}}] loss: {loss.item(): .3f}, "
+              f"training accuracy: {acc_train: .3f}, val_f1: {f1_val: .3f}")
+
+print("Optimization finished!")
+
+oh = OneHotEncoder(sparse=False)
+train_hierarchy = oh.fit_transform(y_top_train.cpu().numpy().reshape(-1, 1))
+val_hierarchy = oh.fit_transform(y_top_val.cpu().numpy().reshape(-1, 1))
+
+x_train = append_feats(x_train, train_hierarchy)
+x_val = append_feats(x_val, val_hierarchy)
+
+model2 = MLP(x_train.shape[1], len(np.unique(y_train)), [256, 128], dropout=dropout)
+
+criterion = th.nn.CrossEntropyLoss(reduction='mean')
+
+device = th.device('cuda' if th.cuda.is_available() and not CPU_ONLY else 'cpu')
+model2 = model2.to(device).float()
+x_train = x_train.to(device).float()
+y_train = y_train.to(device)
+x_val = x_val.to(device).float()
+
+print(f"x_val shape: {x_val.shape}")
+print(f"x_train shape: {x_train.shape}")
+
+# optimizer needs to be constructed AFTER the model was moved to GPU
+optimizer = th.optim.Adam(model2.parameters(), lr=lr)
+length = len(str(epochs))
+print("### Training start (Top-Level)! ###")
+for epoch in range(epochs):
+    model2.train()
+    outputs = model2(x_train)
     loss = criterion(outputs, y_train)
     # performance tip: try set_to_none=True
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
-    model.eval()
+    model2.eval()
     with th.no_grad():
-        logits = model(x_train)
-        logits_val = model(x_val)
+        logits = model2(x_train)
+        logits_val = model2(x_val)
         pred_val = np.argmax(logits_val.cpu().numpy(), axis=1)
         pred_train = np.argmax(logits.cpu().numpy(), axis=1)
         f1_val = f1_score(y_val.cpu(), pred_val, average='macro')
@@ -153,10 +212,17 @@ for epoch in range(epochs):
 
 print("Optimization finished!")
 
+del x_train
+del x_val
+del y_train
+del y_val
+
 with th.no_grad():
     x_test = x_test.to(device)
-    pred_test = np.argmax(model(x_test.cpu().detach().numpy(), axis=1))
-    acc_test = accuracy_score(y_test.cpu().detach(), pred_test)
+    top_pred = th.nn.Softmax()(model1(x_test))
+    x_test = append_feats(x_test, top_pred).to(device)
+    pred_test = np.argmax(model2(x_test).cpu(), axis=1)
+    acc_test = accuracy_score(y_test.cpu(), pred_test)
     f1 = f1_score(y_test.cpu().detach(), pred_test, average='macro')
 
 print(f"Test Accuracy: {acc_test: .3f}")
@@ -165,7 +231,7 @@ print(f"F1-Macro: {f1: .3f}")
 
 if save_results:
     i = df.index.max() + 1 if df.index.max() != np.nan else 0
-    df.loc[i] = {'seed': seed, 'model': "MLP", 'hierarchy': "flat", 'f1-macro': f1,
+    df.loc[i] = {'seed': seed, 'model': "MLP", 'hierarchy': "per-level", 'f1-macro': f1,
                  'accuracy': acc_test}
     df.to_csv(result_file)
 
